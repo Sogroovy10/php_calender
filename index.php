1985bd9230d7249171f955bd2186c43a28fe3f2c
Jun Tagaya <nujtgy@gmail.com>
2020-03-07 00:49:36 +0900

    $matches配列の利用
    ①checkdate関数の引数を、日付文字列から切り出していたが、preg_match関数の結果得られた$matches配列を利用するように変更。
    ②htmlの属性は基本的にダブルクオーテーション、phpの文字列は基本的にシングルクオテーションとするように統一

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 function validate_date($date)
 {
   //フォーマットが正しいことを確認する。
-  if(preg_match('/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/',$date,$mathes)) {
+  if(preg_match('/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/',$date,$matches)) {
     //checkdate関数を用いて日付として許容かチェック
-    return checkdate((int) substr($date,5,2),(int) substr($date,8,2),(int) substr($date,0,4));
+    //checkdateの引数はint型でmonth,day,year。preg_matchで得られた$matchesの配列を利用する
+    return checkdate((int) $matches[2],(int) $matches[3],(int) $matches[1]);
   }else{
     return false;
   }
@@ -30,18 +31,18 @@ if(isset($_GET['action'])){
 // この配列で、第1週当月1日までに前月が何日含まれるかを導出し、日付開始位置の調整に用いる
 // 例えば、上記のように1日が水曜日だった場合、日～火には前月の最後の3日を設定する。
 $week_array = [
-  "Sunday" => 0,
-  "Monday" => 1,
-  "Tuesday"=> 2,
-  "Wednesday" => 3,
-  "Thursday" => 4,
-  "Friday" => 5,
-  "Saturday" => 6,
+  'Sunday' => 0,
+  'Monday' => 1,
+  'Tuesday'=> 2,
+  'Wednesday' => 3,
+  'Thursday' => 4,
+  'Friday' => 5,
+  'Saturday' => 6,
 ];
 //当月1日取得(この時点で1日を設定しておく。)
-$date->modify("first day of this month");
+$date->modify('first day of this month');
 //カレンダー開始日を補正するための日数を取得
-$start_date_correction = - $week_array[$date->format("l")];
+$start_date_correction = - $week_array[$date->format('l')];
 
 // 当月以外の日付は薄いグレーで出力するために、当月の値を退避
 $current_month = $date->format('M');
@@ -71,9 +72,9 @@ $current_month = $date->format('M');
       ?>
     <!-- ボタンの設定 -->
       <form action="index.php" method="get">
-        <button type='submit' name='action' value=<?php echo $previous_month; ?>>先月</button>
-        <button type='submit' name='action' value=<?php echo $this_month; ?> >今月</button>
-        <button type='submit' name='action' value=<?php echo $next_month; ?> >翌月</button>
+        <button type="submit" name="action" value=<?php echo $previous_month; ?>>先月</button>
+        <button type="submit" name="action" value=<?php echo $this_month; ?> >今月</button>
+        <button type="submit" name="action" value=<?php echo $next_month; ?> >翌月</button>
       </form>
       <!-- カレンダー日付の表示 -->
       <?php
@@ -95,15 +96,15 @@ $current_month = $date->format('M');
           <tr>
           <?php for ($j=0; $j<7; $j++): ?>
           <!-- 当月以外の日付は薄いグレーで出力 -->
-            <?php if($date->format("M")==$current_month): ?>
+            <?php if($date->format('M')==$current_month): ?>
               <!-- 日曜日は赤字 -->
-              <?php if($date->format("l")=="Sunday"): ?>
-                <td class="sun"><?php echo $date->format("d"); ?></td>
+              <?php if($date->format('l')=='Sunday'): ?>
+                <td class="sun"><?php echo $date->format('d'); ?></td>
               <?php else: ?>
-                <td><?php echo $date->format("d"); ?></td>
+                <td><?php echo $date->format('d'); ?></td>
               <?php endif; ?>
             <?php else: ?>
-              <td class="grey"><?php echo $date->format("d"); ?></td>
+              <td class="grey"><?php echo $date->format('d'); ?></td>
             <?php endif; ?>
             <!-- 日付カウントアップ -->
             <?php $date->modify('+1 day'); ?>
